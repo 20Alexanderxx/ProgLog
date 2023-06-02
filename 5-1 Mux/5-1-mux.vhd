b100730cfdc5a4dbcb4495c50bcf4aeac2fa2579
20Alexanderxx <a.schmidt.2002@gmx.de>
2023-06-02 10:41:44 +0200

    added 5to1 Mux source file

---
+++ 5-1 Mux/5-1-mux.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux4to1 is
+	port( y, x, w, v, u, s2, s1, s0 : in  std_logic;
+		   m                      : out std_logic);
+end mux4to1;
+
+architecture behave of mux4to1 is
+begin
+	process(s2, s1, s0)
+	begin
+    if (s2='0') then
+  		if (s1='0' And s0='0') then
+  			m <= u;
+  		elsif (s1='0' And s0='1') then
+  			m <= v;
+  		elsif (s1='1' And s0='0') then
+  			m <= w;
+  		else
+  			m <= x;
+  		end if;
+  	elsif (s2='1')
+  	  m <= y
+    end if;
+	end process;	
+end behave;
