31bb4278a4a0f174b725903974f1b7c4688becb9
Schaf <timhendrik01@gmail.com>
2023-06-14 15:38:21 +0200

    added Testbench and framework, fixed BCD_Counter

--- BCD-Counter/BCD_Counter.vhd
+++ BCD-Counter/BCD_Counter.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity BCD_Counter is
 	port( reset, clk : in std_logic;
-         Op      : out std_logic_vector(3 downto 0));
+         Qp      : out std_logic_vector(3 downto 0));
 end BCD_Counter;
 
 architecture beehive of BCD_Counter is
@@ -12,26 +12,26 @@ component async_JK_FF is
 			Qp, Qn			: out std_logic);
 end component;
 
-signal j_s, k_s, op_s, on_s : std_logic_vector(3 downto 0);
+signal j_s, k_s, qp_s, qn_s : std_logic_vector(3 downto 0);
 constant numerounoundnichnumerodos : std_logic := '1';
 
 begin 
-	FlipFlop0: async_JK_FF port map(j_s(0), k_s(0), clk. reset, op_s(0), on_s(0));
-	FlipFlop1: async_JK_FF port map(j_s(1), k_s(1), clk. reset, op_s(1), on_s(1));
-	FlipFlop2: async_JK_FF port map(j_s(2), k_s(2), clk. reset, op_s(2), on_s(2));
-	FlipFlop3: async_JK_FF port map(j_s(3), k_s(3), clk. reset, op_s(3), on_s(3));
+	FlipFlop0: async_JK_FF port map(j_s(0), k_s(0), clk, reset, qp_s(0), qn_s(0));
+	FlipFlop1: async_JK_FF port map(j_s(1), k_s(1), clk, reset, qp_s(1), qn_s(1));
+	FlipFlop2: async_JK_FF port map(j_s(2), k_s(2), clk, reset, qp_s(2), qn_s(2));
+	FlipFlop3: async_JK_FF port map(j_s(3), k_s(3), clk, reset, qp_s(3), qn_s(3));
 	
-	j_s(3) <= op_s(0) and op_s(1) and op_s(2);
-	k_s(3) <= op_s(0) or op_s(1) or op_s(2);
+	j_s(3) <= qp_s(0) and qp_s(1) and qp_s(2);
+	k_s(3) <= qp_s(0) or qp_s(1) or qp_s(2);
 	
-	j_s(2) <= op_s(0) and op_s(1) and on_s(3);
-	k_s(2) <= op_s(3) or (op_s(0) and op_s(1));
+	j_s(2) <= qp_s(0) and qp_s(1) and qn_s(3);
+	k_s(2) <= qp_s(3) or (qp_s(0) and qp_s(1));
 	
-	j_s(1) <= op_s(0) and on_s(3);
-	k_s(1) <= op_s(0) or op_s(3);
+	j_s(1) <= qp_s(0) and qn_s(3);
+	k_s(1) <= qp_s(0) or qp_s(3);
 	
-	j_s(0) <= on_s(3) or (on_s(1) and on_s(2));
+	j_s(0) <= qn_s(3) or (qn_s(1) and qn_s(2));
 	k_s(0) <= numerounoundnichnumerodos;
 	
-	Op <= op_s;
+	Qp <= qp_s;
 end beehive;
---
+++ BCD-Counter/bcdcounter_tb.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity bcdcounter_tb is
+end bcdcounter_tb;
+
+architecture testbench of bcdcounter_tb is
+component Counter_framework is
+	port( clk, clr : in std_logic;
+         Qp      : out std_logic_vector(0 to 6));
+end component;
+
+signal s_CLK : std_logic := '1';
+signal s_CLR : std_logic;
+signal s_7Seg: std_logic_vector(0 to 6);
+
+begin
+	dut: Counter_framework port map (s_CLK, s_CLR, s_7Seg);
+	
+	process
+	begin
+		s_CLK <= NOT s_CLK;
+		wait for 5 ns;
+	end process;
+	
+	process
+	begin
+		s_CLR <= '1';
+		wait for 12 ns;
+		s_CLR <= '0';
+		wait for 10 ns;
+		s_CLR <= '1';
+		wait;
+	end process;
+	
+end testbench;
