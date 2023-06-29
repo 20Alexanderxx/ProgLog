221b5fe613c7ccfa8254e800f11c1e099adc83bf
Schaf <timhendrik01@gmail.com>
2023-06-29 16:17:00 +0200

    fixed little mistake

--- BCD-Uhr/BCD_24_Counter.vhd
+++ BCD-Uhr/BCD_24_Counter.vhd
@@ -33,7 +33,7 @@ signal qp_out : std_logic_vector(5 downto 0);
 begin 
 	clr_out <= clr_in or reset;
 	CNT10: BCD_10_Counter port map(clr_out, clk, sen, sval_l, qp_out(3 downto 0));
-	DFF : async_D_FF port map(clr_out, clk, zerrro, clk_q, qn);
+	DFF : async_D_FF port map(check, clk, zerrro, clk_q, qn);
 	CNT6: BCD_3_Counter port map(clr_out, clk_q, sen, sval_h, qp_out(5 downto 4));
 	process (qp_out)
 	begin
